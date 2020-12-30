e7a7d1ed99b9bf39205e7f74aba1113cf9957b0f
Xiangcheng Kuo <xiangcheng.guo@fastonetech.com>
2020-12-30 16:34:39 +0800

    first commit

---
+++ src/club/geek66/clojure/web/core.clj
@@ -0,0 +1,44 @@
+(ns club.geek66.clojure.web.core
+	(:require [org.httpkit.server :as server]
+			  [compojure.core :refer :all]
+			  [compojure.route :as route]
+			  [ring.middleware.defaults :refer :all]
+			  [clojure.pprint :as pp]
+			  [clojure.string :as str]
+			  [clojure.data.json :as json])
+	(:gen-class))
+
+; Simple Body Page
+(defn simple-body-page [req]
+	{:status  200
+	 :headers {"Content-Type" "text/html"}
+	 :body    "Hello World"})
+
+; request-example
+(defn request-example [req]
+	{:status  200
+	 :headers {"Content-Type" "text/html"}
+	 :body    (->>
+				  (pp/pprint req)
+				  (str "Request Object: " req))})
+
+(defn request-body [req]
+	{:status  200
+	 :headers {"Content-Type" "application/json"}
+	 :body    (json/write-str {:name "Alice"})})
+
+(defroutes app-routes
+	(GET "/" [] simple-body-page)
+	(GET "/request" [] request-example)
+	(GET "/body" [] request-body)
+	(route/not-found "Error, page not found!"))
+
+(defn -main
+	"This is our main entry point"
+	[& args]
+	(let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+		; Run the server with Ring.defaults middleware
+		(server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+		; Run the server without ring defaults
+		;(server/run-server #'app-routes {:port port})
+		(println (str "Running webserver at http:/127.0.0.1:" port "/"))))
